87ef5091cd2287d3bda5ef270d20c69fe3781b17
roshan <roshan.prakash.r@gmail.com>
2014-10-07 19:25:31 +0100

    Initial Commit

---
+++ MCMS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MCMS
+//
+//  Created by roshan on 07/10/2014.
+//  Copyright (c) 2014 learningIOS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MCMS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MCMS
+//
+//  Created by roshan on 07/10/2014.
+//  Copyright (c) 2014 learningIOS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
